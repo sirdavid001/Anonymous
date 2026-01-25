40728f003e851f3427b4c8cf78cd4010b4c129ba
sirdavid <itssirdavid@gmail.com>
2026-01-25 05:38:05 -0600

    Fix SMTP config for Brevo (TLS/SSL)

--- app.py
+++ app.py
@@ -35,8 +35,9 @@ app.config["MAX_CONTENT_LENGTH"] = 250 * 1024 * 1024  # 250MB
 from flask_mail import Mail
 
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", "587"))
 app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
